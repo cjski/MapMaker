9775a03961e21c2329d31235651f9e205cc3cac9
Inninte <adrianyabut1998@gmail.com>
2017-09-16 07:01:46 -0400

    Added buttons to change modes

--- app/src/main/java/JanowskiLuoYabut/HackTheNorth/MapMaker/MainActivity.java
+++ app/src/main/java/JanowskiLuoYabut/HackTheNorth/MapMaker/MainActivity.java
@@ -10,7 +10,9 @@ import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewConfiguration;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.ByteArrayOutputStream;
@@ -18,18 +20,50 @@ import java.io.ByteArrayOutputStream;
 public class MainActivity extends AppCompatActivity {
 
     private long startClickTime;
+    private int currentMode = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initViews();
+    }
+
+    public void initViews() {
+        final TextView modeTextView = (TextView) findViewById(R.id.text_view_mode);
+        modeTextView.setText("Draw Mode");
+        Button drawModeButton = (Button) findViewById(R.id.draw_mode_button);
+        drawModeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                currentMode = 1;
+                modeTextView.setText("Draw Mode");
+            }
+        });
+        Button nodeModeButton = (Button) findViewById(R.id.node_mode_button);
+        nodeModeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                currentMode = 2;
+                modeTextView.setText("Node Mode");
+            }
+        });
+        Button pathModeButton = (Button) findViewById(R.id.path_mode_button);
+        pathModeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                currentMode = 3;
+                modeTextView.setText("Path Mode");
+            }
+        });
 
         final ImageView iv = (ImageView) findViewById(R.id.map_image);
         // Create a MUTABLE bitmap
-        final Bitmap bm = getMutableBitmap(getResources(), R.drawable.map_colour);
-        final Bitmap bm2 = Bitmap.createScaledBitmap(bm, 2500, 2500, false);
-        iv.setImageBitmap(bm2);
+        final Bitmap originalBitMap = getMutableBitmap(getResources(), R.drawable.map_colour);
+        // A second bitmap is generated for sizing purposes.  This is the bitmap that will be used for everything
+        final Bitmap resizedBitMap = Bitmap.createScaledBitmap(originalBitMap, 4000, 4000, false);
+        iv.setImageBitmap(resizedBitMap);
 
         // Listens for touch events in the ImageView
         View.OnTouchListener otl = new View.OnTouchListener() {
@@ -46,29 +80,32 @@ public class MainActivity extends AppCompatActivity {
                 int xInt = Math.round(pts[0]);
                 int yInt = Math.round(pts[1]);
 
-                // The following conditionals allow differentiation between a tap and a drag gesture
-                if (event.getAction() == MotionEvent.ACTION_DOWN) {
-
-                    startClickTime = System.currentTimeMillis();
-
-                } else if (event.getAction() == MotionEvent.ACTION_UP) {
-
-                    if (System.currentTimeMillis() - startClickTime < ViewConfiguration.getTapTimeout()) {
-
-                        for (int i = 0; i < xInt; i++) {
-                            for (int j = 0; j < yInt; j++) {
-                                bm2.setPixel(i, j, Color.rgb(255, 0, 0));
+                if (currentMode == 1) {
+                    // The following conditionals allow differentiation between a tap and a drag gesture
+                    if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                        startClickTime = System.currentTimeMillis();
+                    } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                        if (System.currentTimeMillis() - startClickTime < ViewConfiguration.getTapTimeout()) {
+                            for (int i = 0; i < xInt; i++) {
+                                for (int j = 0; j < yInt; j++) {
+                                    resizedBitMap.setPixel(i, j, Color.rgb(255, 0, 0));
+                                }
                             }
+                            iv.setImageBitmap(resizedBitMap);
+                        } else {
+                            return false;
                         }
-                        iv.setImageBitmap(bm2);
                     }
-
+                } else if (currentMode == 2) {
+                    Toast.makeText(MainActivity.this, "Will be implemented later", Toast.LENGTH_SHORT).show();
+                } else if (currentMode == 3) {
+                    Toast.makeText(MainActivity.this, "Shall be implemented too", Toast.LENGTH_SHORT).show();
                 }
+
                 return true;
             }
         };
         iv.setOnTouchListener(otl);
-
     }
 
     // Creates mutable bitmap
